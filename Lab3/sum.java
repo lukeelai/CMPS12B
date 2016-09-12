a96898001ce1e1335a4051ea08b79eafff64fd2a
Luke Lai <uklelai@gmail.com>
2016-09-12 16:44:56 -0700

    Create sum.java

---
+++ Lab3/sum.java
@@ -0,0 +1,30 @@
+//Luke Lai
+//1355154
+//10/22/14
+//Sum.java
+//Define a class for adding numbers
+
+public class Sum {
+	//Keep track of running total
+	private int value;
+	
+	//Constructor
+	public Sum(){
+		value = 0;
+	}
+
+	//Get current value
+	public int getValue(){
+		return value;
+	}
+	
+	//Add something to running total
+	public void add(int v){
+		value += v;
+	}
+
+	//Reset total
+	public void reset() {
+		value = 0;
+	}
+}
