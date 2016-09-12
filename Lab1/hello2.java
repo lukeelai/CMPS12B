7afa10e052036ae4239ff6893a8a7ccd28311273
Luke Lai <uklelai@gmail.com>
2016-09-12 16:40:38 -0700

    Create hello2.java

---
+++ Lab1/hello2.java
@@ -0,0 +1,18 @@
+//Luke Lai
+//1355154
+//cmps012b
+//10/9/14
+//hello2.java
+//Prints a poem to stdout
+
+import static java.lang.System.*;
+
+class hello{
+
+    public static void main( String[] args){
+	System.out.println("Roses are red,");
+	System.out.println("Violets are blue,");
+	System.out.println("Computers are fun,");
+	System.out.println("Learning is too!");
+    }
+}
